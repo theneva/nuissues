21824e6c32079528690c8f399cb1b2d58f431712
Martin Lehmann <lehmar11@student.westerdals.no>
2015-05-06 01:08:40 +0200

    Accept MONGOLAB_URI for MongoDB URI

--- server/db.js
+++ server/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-var uri = process.env.MONGO_URI || 'mongodb://localhost/nuissues';
+var uri = process.env.MONGO_URI
+	|| process.env.MONGOLAB_URI
+	|| 'mongodb://localhost/nuissues';
 
 mongoose.connect(uri, function() {
 	console.log('Connected to MongoDB on URI', uri);
